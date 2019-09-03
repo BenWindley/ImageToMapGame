a6e1fd7bdbd1f0c77a1d8f2fc551f28082205b49
Ben Windley <ben.windley@yahoo.co.uk>
2019-09-03 23:34:44 +0100

    Map reading from image added

---
+++ Unity Project/Assets/MapGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public List<Texture2D> maps;
+    public Texture2D map;
+    public ImageReader.types[] types_array;
+
+    public Color void_colour;
+    public Color wall_colour;
+    public Color pac_colour;
+    public Color ghost_colour;
+
+
+    void Start ()
+    {
+		if(maps.Count > 0)
+        {
+            map = maps[Random.Range(0, maps.Count)];
+
+            types_array = ImageReader.GetImageData(
+                map,
+                void_colour,
+                wall_colour,
+                pac_colour,
+                ghost_colour
+                );
+        }
+        else
+        {
+            Debug.Log("No Maps");
+        }
+	}
+
+	void Update ()
+    {
+		
+	}
+}
---
+++ Unity Project/Assets/Scripts/ImageReader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ImageReader
+{
+    public enum types
+    {
+        VOID,
+        WALL,
+        PACSPAWN,
+        GHOSTSPAWN
+    }
+
+    public static types[] GetImageData(
+        Texture2D image,
+        Color void_col,
+        Color wall_col,
+        Color pac_col,
+        Color ghost_col)
+    {
+        types[] image_data = new types[image.width * image.height];
+
+        for(int y = 0; y < image.height; ++y)
+        {
+            for(int x = 0; x < image.width; ++x)
+            {
+                if(image.GetPixel(x,y) == void_col)
+                {
+                    image_data[x + y * image.width] = types.VOID;
+                }
+                else if(image.GetPixel(x, y) == wall_col)
+                {
+                    image_data[x + y * image.width] = types.WALL;
+                }
+                else if (image.GetPixel(x, y) == pac_col)
+                {
+                    image_data[x + y * image.width] = types.PACSPAWN;
+                }
+                else if (image.GetPixel(x, y) == ghost_col)
+                {
+                    image_data[x + y * image.width] = types.GHOSTSPAWN;
+                }
+                else
+                {
+                    Debug.Log("Unrecognised Colour");
+
+                    image_data[x + y * image.width] = types.VOID;
+                }
+            }
+        }
+
+        return image_data;
+    }
+}
